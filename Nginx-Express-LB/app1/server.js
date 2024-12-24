5819911d1b9231ae836733159a59898659b2b4dd
prashantr2 <prashantrawat2ofc@gmail.com>
2024-12-25 00:40:51 +0530

    Added nginx-lb with round-robin balancing for 2 dummy express apps

---
+++ Nginx-Express-LB/app1/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+    
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    console.log(`Landed on home`);
+    res.render('ui.ejs')
+});
+
+app.post('/', (req, res) => {
+    console.log(`Sending: ${req.body.msg}`);
+    res.render('ui.ejs');
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server up and runing on port: ${PORT}`);
+})
